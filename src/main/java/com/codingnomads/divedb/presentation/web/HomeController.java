c772884c8a65145606d28ebd46f05e6cb5f3efd5
Roi Otero Morales <roioteromorales@gmail.com>
2018-11-14 12:37:51 -0500

    Bunch of stuff

--- src/main/java/com/codingnomads/divedb/presentation/web/HomeController.java
+++ src/main/java/com/codingnomads/divedb/presentation/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.codingnomads.divedb.presentation.web;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,8 @@ import java.time.LocalDate;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @Cacheable
+    @GetMapping({"/","/home"})
     public String home(Model model) {
         model.addAttribute("date", LocalDate.now());
         return "home";
